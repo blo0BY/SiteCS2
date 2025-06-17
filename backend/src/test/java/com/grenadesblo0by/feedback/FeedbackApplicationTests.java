827dc8fdbd2e5b17ecd4fc53b944e7f8be8cd366
blo0BY <130252602+blo0BY@users.noreply.github.com>
2025-06-17 19:32:31 +0300

    Add files via upload

---
+++ backend/src/test/java/com/grenadesblo0by/feedback/FeedbackApplicationTests.java
@@ -0,0 +1,11 @@
+package com.grenadesblo0by.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FeedbackApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
